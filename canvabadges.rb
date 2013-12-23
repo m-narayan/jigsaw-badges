c252dbca80fd050c16c2250282aa90539dbc0e47
sukumar <sukumarreddy@arrivusystems.com>
2013-12-23 10:30:48 +0530

    Add production.yml

--- canvabadges.rb
+++ canvabadges.rb
@@ -36,11 +36,10 @@ class Canvabadges < Sinatra::Base
   # enable sessions so we can remember the launch info between http requests, as
   # the user takes the assessment
   enable :sessions
-  #raise "session key required" if ENV['RACK_ENV'] == 'production' && !ENV['SESSION_KEY']
-  #set :session_secret, ENV['SESSION_KEY'] || "local_secret"
-  set :session_secret, "local_secret"
 
-  env = ENV['RACK_ENV'] || settings.environment
+  raise "session key required" if settings.environment == 'production' && !settings.environment
+  set :session_secret, settings.session_key || "local_secret"
+
   #DataMapper.setup(:default, (ENV["DATABASE_URL"] || "sqlite3:///#{Dir.pwd}/#{env}.sqlite3"))
   DataMapper.setup(:default, (settings.database_url || "sqlite3:///#{Dir.pwd}/#{env}.sqlite3"))
   DataMapper.auto_upgrade!
@@ -48,6 +47,7 @@ class Canvabadges < Sinatra::Base
   configure :production do
     require 'rack-ssl-enforcer'
     use Rack::SslEnforcer
+
   end
 end
 
